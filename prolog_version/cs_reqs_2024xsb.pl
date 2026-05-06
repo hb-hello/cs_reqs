756c6c9cb7c700e361b473331e24e4ee911303da
hb-hello <biyanihritwik@gmail.com>
2026-05-06 18:06:40 -0400

    prolog stuff.

--- prolog_version/cs_reqs_2024xsb.pl
+++ prolog_version/cs_reqs_2024xsb.pl
@@ -27,7 +27,7 @@ upperdivCS(Id) :-
 %atom_concat(Prefix, Postfix, Full) :-
 %  concat_atom([Prefix, Postfix], Full).
 
-measure_run_xsb(Goal, T) :-
+measure_run_xsb(Goal) :-
   statistics(runtime, [_,_]),
   (call(Goal) -> Outcome = yes ; Outcome = no),
   statistics(runtime, [_,T]),
