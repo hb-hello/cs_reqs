8361f61f75d7a709ff0428da8e351f113c95b263
Ethan H <jujugregoryh@gmail.com>
2026-05-26 12:50:03 -0400

    simplify elect. use aggregate_all for findall and count; add courses(Cid, Item) as in clingo.

--- prolog_version/cs_reqs_2024swi.pl
+++ prolog_version/cs_reqs_2024swi.pl
@@ -3,7 +3,7 @@ subseq([H|T], [H|Sub]) :- subseq(T, Sub).
 subseq([_|T], Sub) :- subseq(T, Sub).
 
 is_higher(Grade, Grade2) :- grade_points(Grade, Points), grade_points(Grade2, Points2), Points >= Points2.
-c_or_higher(Grade) :- is_higher(Grade, 'C').
+is_c_or_higher(Grade) :- is_higher(Grade, 'C').
 
 %% mapping letter grade to points for GPA calculation
 grade_points('A', 4.0). grade_points('A-', 3.67).
@@ -12,7 +12,7 @@ grade_points('C+', 2.33). grade_points('C', 2.0). grade_points('C-', 1.67).
 grade_points('D+', 1.33). grade_points('D', 1.0). grade_points('D-', 0.67).
 grade_points('F', 0.0).
 
-passed(Id) :- taken(Id, _, Grade, _, _), c_or_higher(Grade).
+passed(Id) :- taken(Id, _, Grade, _, _), is_c_or_higher(Grade).
 
 % passed all courses with course Id in Subject
 passed_all(Subject) :- forall(c(Subject, Id), passed(Id)).
@@ -22,7 +22,9 @@ passed_all(Subject, ReqData) :- forall(c(Subject, Id), memberchk([Id, _, _], Req
 
 :- dynamic(wit/2).
 % course C is witness for passing all courses in a subject in requirement Item
-wit(Item, Id) :- s(Item, Subj), passed_all(Subj), c(Subj, Id).
+wit(I, Cid) :- item(I), s(I, Subj), passed_all(Subj), c(Subj, Cid).
+
+courses(Cid, I) :- item(I), s(I, Subj), c(Subj, Cid).
 
 :- dynamic(c/2).
 :- dynamic(s/2).
@@ -36,24 +38,16 @@ c(dmath2, 'CSE 150').
 c(sys, 'CSE 220').
 s(intro, prog). s(intro, prog2). s(intro, dmath). s(intro, dmath2). s(intro, sys).
 
-intro_courses(Id) :-
-  c(prog, Id); c(prog2, Id); c(dmath, Id); c(dmath2, Id); c(sys, Id).
-
 req(intro) :-
   (passed_all(prog); passed_all(prog2)),
   (passed_all(dmath); passed_all(dmath2)),
   passed_all(sys).
 
 % 2. Required Advanced Courses
+c(theory, 'CSE 303'). c(theory2, 'CSE 350').
+c(algo, 'CSE 373'). c(algo2, 'CSE 385').
 c(other, 'CSE 310'). c(other, 'CSE 316'). c(other, 'CSE 320'). c(other, 'CSE 416').
-c(algo, 'CSE 373').
-c(algo2, 'CSE 385').
-c(theory, 'CSE 303').
-c(theory2, 'CSE 350').
 s(adv, theory). s(adv, theory2). s(adv, algo). s(adv, algo2). s(adv, other).
-
-advanced_courses(Id) :-
-  c(other, Id); c(algo, Id); c(algo2, Id); c(theory, Id); c(theory2, Id).
 
 req(adv) :-
   (passed_all(algo); passed_all(algo2)),
@@ -61,50 +55,44 @@ req(adv) :-
   passed_all(other).
 
 % 3. Computer Science Electives  %% simpler than 2025
-elective_exclude('CSE 475').
-elective_exclude('CSE 495').
-elective_exclude('CSE 496').
-elective_exclude('CSE 301').
-elective_exclude('CSE 300').
-elective_exclude('CSE 312').
+c(elect_exclude, 'CSE 475'). c(elect_exclude, 'CSE 495'). c(elect_exclude, 'CSE 496'). c(elect_exclude, 'CSE 301'). c(elect_exclude, 'CSE 300'). c(elect_exclude, 'CSE 312').
 
-req(elect) :- findall(Id, (passed(Id), elective(Id)), Electives),
-  length(Electives, Count),
-    Count > 3.
-
-elective(Id) :- \+ advanced_courses(Id),
-    taken(Id, Cr, _, _, _),
-    Cr >= 3,
-    \+ elective_exclude(Id),
-    upperdivCS(Id).
-
-upperdivCS(Id) :- 
+cse_upper_division(Id) :- 
   atom_concat('CSE ', CourseNumstr, Id),
   atom_number(CourseNumstr, CourseNumInt),
   CourseNumInt >= 300.
 
-wit(elect, Id) :- passed(Id), elective(Id).
+elect_passed(Id) :- 
+  taken(Id, Cr, G, _, _), cse_upper_division(Id), 
+  is_c_or_higher(G), Cr >= 3,
+  \+ courses(Id, adv), \+ c(elect_exclude, Id).
+
+req(elect) :-
+  aggregate_all(count, distinct(Id, elect_passed(Id)), Count), Count >= 4.
+
+wit(elect, Id) :- elect_passed(Id).
 
 % Req 4. Calculus
 c(calc, 'AMS 151'). c(calc, 'AMS 161').
 c(calc2, 'MAT 125'). c(calc2, 'MAT 126'). c(calc2, 'MAT 127').
 c(calc3, 'MAT 131'). c(calc3, 'MAT 132').
 s(calc, calc). s(calc, calc2). s(calc, calc3).
-req(calc) :-
-  (passed_all(calc); passed_all(calc2); passed_all(calc3)).
+
+req(calc) :- passed_all(calc); passed_all(calc2); passed_all(calc3).
 
 % Req 5. Linear Algebra
 c(alg1, 'MAT 211').
 c(alg2, 'AMS 210').
 s(alg, alg1). s(alg, alg2).
-req(alg) :-
-  (passed_all(alg1); passed_all(alg2)).
+
+req(alg) :- passed_all(alg1); passed_all(alg2).
 
 % Req 6. Statistics / Finite Math
 c(fmath, 'AMS 301').
 c(sta1, 'AMS 310').
 c(sta2, 'AMS 311').
 s(sta, fmath). s(sta, sta1). s(sta, sta2).
+
 req(sta) :-
   passed_all(fmath),
   (passed_all(sta1); passed_all(sta2)).
@@ -136,16 +124,17 @@ sci_taken(Id) :- distinct(Id,
 
 req_sci_combs(ReqData) :- s(sci_combs, Subj), passed_all(Subj, ReqData).
 
+%% TODO: this is more general than sci requirement, might want to move it to the top and use it other requirements as well.
 best_taken(Id, Cr, Grade, When, Where) :- 
     taken(Id, Cr, Grade, When, Where), 
     grade_points(Grade, GPts),
     \+ (taken(Id, _, BetterGrade, _, _), grade_points(BetterGrade, BGPts), BGPts > GPts).
 
 req(sci) :-
-  findall(
-    [Id, Cr, Grade], 
-    (sci_taken(Id), once(best_taken(Id, Cr, Grade, _, _)), grade_points(Grade, _)), 
-    SciData),
+  findall([Id, Cr, Grade], 
+          (sci_taken(Id), once(best_taken(Id, Cr, Grade, _, _)), % once/1 break ties if repeats have same grade
+                          grade_points(Grade, _)), 
+          SciData),
   once((subseq(SciData, SciReqData),
         req_sci_combs(SciReqData),
         aggregate_all(sum(Cr), member([_, Cr, _], SciReqData), SciCreds),
@@ -155,32 +144,36 @@ req(sci) :-
 
 wit(sci, Id) :- sci_taken(Id).
 
-items123_course(Id) :- intro_courses(Id) ; advanced_courses(Id) ; elective(Id).
-items23_course(Id) :- advanced_courses(Id) ; elective(Id).
+% ethics and communication courses
+c(ethics, 'CSE 312'). 
+s(ethics, ethics).
+
+req(ethics) :- passed_all(ethics).
+
+c(writing, 'CSE 300').
+s(writing, writing).
+
+req(writing) :- passed_all(writing).
+
+items123_course(Cid) :- courses(Cid, intro) ; courses(Cid, adv) ; elect_passed(Cid).
+items23_course(Cid) :- courses(Cid, adv) ; elect_passed(Cid).
 
 items123_credits(Crs) :-
-  aggregate_all(
-  sum(Cr),
-  (taken(Cid, Cr, _, _, 'SB'), passed(Cid), items123_course(Cid)),
-  Crs).
+  aggregate_all(sum(Cr),
+                (taken(Cid, Cr, _, _, 'SB'), passed(Cid), items123_course(Cid)),
+                Crs).
 
 items23_credits(Crs) :-
-  aggregate_all(
-  sum(Cr),
-  (taken(Cid, Cr, _, _, 'SB'), passed(Cid), items23_course(Cid)),
-  Crs).
+  aggregate_all(sum(Cr),
+                (taken(Cid, Cr, _, _, 'SB'), passed(Cid), items23_course(Cid)),
+                Crs).
 
 req(credits_at_sb) :-
   items123_credits(Items123_credit), Items123_credit >= 24,
   items23_credits(Items23_credit), Items23_credit >= 18.
 
+%% TODO: items123 includes items23. only need to include items123.
 wit(credits_at_sb, Id) :- (items123_course(Id); items23_course(Id)), taken(Id, _, _, _, 'SB'), passed(Id).
-
-% ethics and communication courses
-c(ethics, 'CSE 312'). c(writing, 'CSE 300').
-s(ethics, ethics). s(writing, writing).
-req(writing) :- passed_all(writing).
-req(ethics) :- passed_all(ethics).
 
 item(intro). item(adv). item(elect). 
 item(calc). item(alg). item(sta). item(sci).
